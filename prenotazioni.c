759cf5f5f083d40bf60e679b2baa84bffc8d2a23
FilippoGovoni <filippogovoni98@gmail.com>
2018-12-26 17:50:05 +0100

    file dove mettiamo le prenotazioni

---
+++ prenotazioni.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+
+typedef struct 
+{
+    int numero;
+    char datainizio[20]; 
+    char datafine[20]; 
+}Ombrellone;
+
+int main(void)
+{
+    Ombrellone O[500];
+    int i;
+    FILE *F;
+
+    if((F=fopen("prenotazioni.txt","wt"))==NULL)
+    {   
+        printf("errore apertura file\n");
+        exit(-1);
+    }   
+    for(i=0;i<500;i++)
+    {
+        O[i].numero=0;
+        strcpy(O[i].datainizio,"00/00/0000");
+        strcpy(O[i].datafine,"00/00/0000");
+    }
+    for(i=0;i<500;i++)
+        {
+            fprintf(F,"%d %s %s\n",O[i].numero,O[i].datainizio,O[i].datafine);
+        }
+    fclose(F);
+    return 0;
+}
